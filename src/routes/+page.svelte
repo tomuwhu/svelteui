e98c535457d9539fd984479db7d8c64707f87df5
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-04-23 07:30:07 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { base } from '$app/paths'
 	import { cssvariable } from '@svelteuidev/composables';
-	import { Stack, Space, Button, Title } from '@svelteuidev/core'
+	import { Input, Stack, Space, Button, Title } from '@svelteuidev/core'
 	import { onMount } from 'svelte'
-	var x: number = 0
+	var x: number = 0, r: number = 0, g: number = 0, b: number = 0
 	var mounted: boolean = false
 	onMount(() => (mounted = true))
 	$: Colors = {
-		t1: `rgb(${x},${255-x},${x})`
+		t1: `rgb(${r},${g},${b})`
 	};
 </script>
 
@@ -16,14 +16,31 @@
 	<Title align="center">SvelteUi Template</Title>
 	<Space h={30} />
 	<Stack align="center">
-		<Button class="animation-bounce" on:click={() => x = x < 250 ? x + 10 : 0}>Szám: {x}</Button>
+		<Button class="animation-bounce" on:click={() => x++}>Szám: {x}</Button>
 		<Button variant="outline" color="orange" href="{base}/link">Link</Button>
+		<div class="grid">
+		<div>R</div><Input type="range" bind:value={r} min=0 max=255></Input>
+		<div>G</div><Input type="range" bind:value={g} min=0 max=255></Input>
+		<div>B</div><Input type="range" bind:value={b} min=0 max=255></Input>
+		</div>
+		
 	</Stack>
 	<Space h={30} />
 	<div use:cssvariable={Colors} class="x">Cica</div>
 {/if}
 
 <style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(6, 1fr);
+		border: solid 1px black;
+		padding: 15px;
+		border-radius: 17px;
+	}
+	.grid div {
+		text-align: right;
+		padding: 10px;
+	}
 	.x {
 		color: var(--t1);
 		text-align: center;
